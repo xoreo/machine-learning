c7eaf5ccef31ff14561f12af00255a1ee879d1b7
xoreo <mattnappo@gmail.com>
2019-03-26 21:30:58 -0400

    Plotted the decision regions

---
+++ perceptron/decision_regions.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
+
+    # Setup the marker generator and color map
+    markers = ("s", "x", "o", "^", "v")
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    colorMap = ListedColormap(colors[:len(np.unique(y))])
+
+    # Plot the decision surface
+    x1_min = X[:, 0].min() - 1 # The minimum value for feature 0,  the sepal length,  of the setosa data
+    x1_max = X[:, 0].max() + 1 # The maximum value for feature 0, the sepal length, of the versicolor data
+
+    x2_min = X[:, 1].min() - 1 # The minimum value for feature 1, the petal length, of the versicolor data
+    x2_max = X[:, 1].max() + 1 # The maximum value for feature 1, the petal length, of the versicolor data
+
+    # Create a meshgrid, the aligning coordinates from two vectors, x1 and x2
+    xx1, xx2 = np.meshgrid(
+        np.arange(x1_min, x1_max, resolution), # Return evenly spaces values given an interval
+        np.arange(x2_min, x2_max, resolution)
+    )
+
+    # Set vector Z = to the class label predictions of the corresponding grid points
+    Z = classifier.predict(
+        np.array(
+            [xx1.ravel(), xx2.ravel()]
+        ).T
+    )
+
+    # Reshape the vector Z to be the same size (same number of columns) as the
+    # two-feature matrix so that it can be
+    # graphed & predicted just like the Iris training subset
+    Z = Z.reshape(xx1.shape)
+
+    # Graph the trained (predicted) data
+    plt.contourf(
+        xx1, xx2,
+        Z,
+        alpha = 0.3,
+        cmap = colorMap
+    )
+
+    # Plot the limits of the datasets
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # Plot the class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(
+            x = X[y == cl, 0], # Plot feature 1/0
+            y = X[y == cl, 1], # Plot feature 2/1
+            alpha = 0.8, # Transparency
+            c = colors[idx],
+            marker = markers[idx],
+            label = cl,
+            edgecolor = "black"
+        )
--- perceptron/train.py
+++ perceptron/train.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from perceptron import Perceptron
+from decision_regions import plot_decision_regions
 
 # Load the iris data as a pandas.DataFrame object
 data = pd.read_csv("data/iris.data", header = None)
@@ -14,6 +15,7 @@ print(data.tail())
 # Select the setosa and versicolor data set
 y = data.iloc[0:100, 4].values # Column 4 is the name of the flower
 y = np.where(y == "Iris-setosa", -1, 1) # Select the setosa and assign the class identifiers (class labels)
+print(y)
 
 # Extract the sepal and petal lengths (real traning data)
 # Columns 0 and 2 are the sepal and petal, respectively
@@ -48,6 +50,42 @@ plt.legend(loc = "upper left")
 # Show the plot
 plt.show()
 
-# perceptron = Perceptron(
-    
-# )
+""" -- Start training the data -- """
+
+# Initialize the perceptron algorithm
+perceptron = Perceptron(
+    eta = 0.1,
+    n_iter = 10
+)
+
+# Train the algorithm
+perceptron.fit(X, y)
+
+# Plot the errors over the epochs
+plt.plot(
+    range(1, len(perceptron.errors_) + 1),
+    perceptron.errors_,
+    marker = "o"
+)
+
+# Label the axes
+plt.xlabel("Epochs")
+plt.ylabel("Number of weight updates")
+
+plt.show()
+
+""" -- Display the decision regions -- """
+
+# Plot the decision regions in order to see the data predictions (better)
+plot_decision_regions(
+    X, y, # The data
+    classifier = perceptron
+)
+
+# Display the axes data
+plt.xlabel("sepal length")
+plt.ylabel("petal length")
+plt.legend(loc = "upper left")
+
+# Display the graph
+plt.show()
